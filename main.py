5e38cb176cb36a471321dd0ca3c5256545b11ee0
AriyanMlzm <ryanmolazem@gmail.com>
2025-11-07 17:55:38 +0330

    add missingValues outliers

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils import load_csv, explore_data
+from utils import load_csv, explore_data, detect_missing_values, handle_missing_values, detect_outliers, handle_outliers
 
 datasetPath = './dataset/dataset.csv'
 
@@ -6,7 +6,14 @@ datasetPath = './dataset/dataset.csv'
 def main():
   dataset = load_csv(datasetPath)
   if dataset is not None:
-    explore_data(dataset)
+    numerical_columns, categorical_columns, binary_columns, continuous_columns, multi_categorical_columns = explore_data(
+        dataset)
+    detect_missing_values(dataset)
+    dataset_clean = handle_missing_values(
+        dataset, binary_columns, continuous_columns, multi_categorical_columns, categorical_columns
+    )
+    detect_outliers(dataset_clean, continuous_columns)
+    handle_outliers(dataset_clean, continuous_columns)
 
 
 if __name__ == '__main__':
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,6 @@
 from .explore_data import explore_data
 from .load_csv import load_csv
+from .detect_missing_values import detect_missing_values
+from .handle_missing_values import handle_missing_values
+from .detect_outliers import detect_outliers
+from .handle_outliers import handle_outliers
